1c694871107797c9b4f7182f4ef83fb4c955682c
Roc Koh <roc@Rocs-MacBook-Pro.local>
2017-09-22 13:58:59 +1000

    Initial Commit

---
+++ SImple Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SImple Calculator
+//
+//  Created by Roc Koh on 22/9/17.
+//  Copyright © 2017 Roc Koh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
